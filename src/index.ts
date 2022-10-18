63768359357604ea2dc7fd9b1e5e5fbf12f9dad2
ycxu <ycxu@tencent.com>
2022-10-18 15:57:21 +0800

    feat: 导出更多函数, 方便扩展 Request 类

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './request/request';
 export * from './request/requestError';
+export * from './request/default';
+export * from './request/plugins';
 export * from './request/adapter/wxAdapter';
 export * from './request/adapter/xhrAdapter';
 export * from './types';
